14cba339d5970def55565f34e4da81675e2d7746
Zhafran <cmzhafran@gmail.com>
2025-09-08 17:33:27 +0800

    Revert "update to overrite filter options after post processing"
    This reverts commit 69d19917c6a60a426744e6f5de82809bfea033e8.

--- lib/cunning_document_scanner.dart
+++ lib/cunning_document_scanner.dart
@@ -16,8 +16,6 @@ class CunningDocumentScanner {
     int noOfPages = 100,
     bool isGalleryImportAllowed = false,
     IosScannerOptions? iosScannerOptions,
-    String? filterType,
-    bool? saveInGallery,
   }) async {
     Map<Permission, PermissionStatus> statuses = await [
       Permission.camera,
@@ -27,35 +25,14 @@ class CunningDocumentScanner {
       throw Exception("Permission not granted");
     }
 
-    // Prepare the iosScannerOptions map
-    Map<String, dynamic>? iosOptionsMap;
-    if (iosScannerOptions != null) {
-      iosOptionsMap = {
-        'imageFormat': iosScannerOptions.imageFormat.name,
-        'jpgCompressionQuality': iosScannerOptions.jpgCompressionQuality,
-        'filterType': iosScannerOptions.filterType,
-        'saveInGallery': iosScannerOptions.saveInGallery,
-      };
-    }
-    
-    // Individual parameters take precedence
-    if (filterType != null) {
-      if (iosOptionsMap == null) {
-        iosOptionsMap = {};
-      }
-      iosOptionsMap['filterType'] = filterType;
-    }
-    if (saveInGallery != null) {
-      if (iosOptionsMap == null) {
-        iosOptionsMap = {};
-      }
-      iosOptionsMap['saveInGallery'] = saveInGallery;
-    }
-
     final List<dynamic>? pictures = await _channel.invokeMethod('getPictures', {
       'noOfPages': noOfPages,
       'isGalleryImportAllowed': isGalleryImportAllowed,
-      if (iosOptionsMap != null) 'iosScannerOptions': iosOptionsMap,
+      if (iosScannerOptions != null)
+        'iosScannerOptions': {
+          'imageFormat': iosScannerOptions.imageFormat.name,
+          'jpgCompressionQuality': iosScannerOptions.jpgCompressionQuality,
+        }
     });
     return pictures?.map((e) => e as String).toList();
   }
